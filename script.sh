b2799ea88a9a9045873c0e96c50eabbd8b3990d4
Chris Lamb <lamby@debian.org>
2015-12-13 14:05:45 +0000

    Don't install recommends.
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- script.sh
+++ script.sh
@@ -83,7 +83,7 @@ fi
 
 cat >>Dockerfile <<EOF
 RUN apt-get update && apt-get dist-upgrade --yes
-RUN apt-get install --yes build-essential equivs devscripts git-buildpackage
+RUN apt-get install --yes --no-install-recommends build-essential equivs devscripts git-buildpackage
 
 WORKDIR ${TRAVIS_DEBIAN_WORKDIR}
 
