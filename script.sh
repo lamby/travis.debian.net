6d3f7568a32df52df6e6c995a52a2b8063ebeedc
Free Ekanayaka <free.ekanayaka@gmail.com>
2016-11-26 09:20:19 +0100

    Install lintian by default

--- script.sh
+++ script.sh
@@ -226,7 +226,7 @@ fi
 
 cat >>Dockerfile <<EOF
 RUN apt-get update && apt-get dist-upgrade --yes
-RUN apt-get install --yes --no-install-recommends build-essential equivs devscripts git-buildpackage ca-certificates pristine-tar ${EXTRA_PACKAGES}
+RUN apt-get install --yes --no-install-recommends build-essential equivs devscripts git-buildpackage ca-certificates pristine-tar lintian ${EXTRA_PACKAGES}
 
 WORKDIR $(pwd)
 COPY . .
