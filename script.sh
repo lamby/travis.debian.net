cf2548970a6d7d7ba750a5728be0a44196f5cd60
Chris Lamb <lamby@debian.org>
2017-02-11 11:53:03 +1300

    Hoist status code output out of conditional.

--- script.sh
+++ script.sh
@@ -317,9 +317,9 @@ apt-get install --yes --no-install-recommends autopkgtest autodep8
 
 TEST_RET="0"
 ${TRAVIS_DEBIAN_AUTOPKGTEST_RUN} ${TRAVIS_DEBIAN_BUILD_DIR}/*.changes ${TRAVIS_DEBIAN_AUTOPKGTEST_SEPARATOR} null || TEST_RET="${?}"
+echo "I: ${TRAVIS_DEBIAN_AUTOPKGTEST_RUN} exited with status code \${TEST_RET}" >&2
 if [ "\${TEST_RET}" != "0" ] && [ "\${TEST_RET}" != "2" ]
 then
-	echo "${TRAVIS_DEBIAN_AUTOPKGTEST_RUN} exited \${TEST_RET}" >&2
 	exit \${TEST_RET}
 fi
 EOF
