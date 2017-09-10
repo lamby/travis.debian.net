61b22ae4618f8507c638676f39232441aed404fd
James McCoy <jamessan@jamessan.com>
2017-09-09 22:52:56 -0400

    Set DEB_BUILD_PROFILES for mk-build-deps
    This allows testing changes to dependencies based on build profiles.

--- script.sh
+++ script.sh
@@ -297,7 +297,7 @@ EOF
 fi
 
 cat >>Dockerfile <<EOF
-RUN env DEBIAN_FRONTEND=noninteractive mk-build-deps --install --remove --tool 'apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends --yes' debian/control
+RUN env DEBIAN_FRONTEND=noninteractive DEB_BUILD_PROFILES="${DEB_BUILD_PROFILES:-}" mk-build-deps --install --remove --tool 'apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends --yes' debian/control
 
 RUN rm -f Dockerfile
 RUN git checkout .travis.yml || true
