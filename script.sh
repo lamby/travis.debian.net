bb77c2d39580fdaf591ed4c539bc9420f561accc
Chris Lamb <lamby@debian.org>
2017-09-14 21:51:39 +0100

    Copy build artefacts after running all tests.

--- script.sh
+++ script.sh
@@ -355,11 +355,6 @@ Info "Running build"
 # shellcheck disable=SC2086
 docker run --env=DEB_BUILD_OPTIONS="${DEB_BUILD_OPTIONS:-}" --env=DEB_BUILD_PROFILES="${DEB_BUILD_PROFILES:-}" ${ARGS} "${TAG}"
 
-Info "Copying build artefacts to ${TRAVIS_DEBIAN_TARGET_DIR}"
-mkdir -p "${TRAVIS_DEBIAN_TARGET_DIR}"
-docker cp "$(cat "${CIDFILE}")":"${TRAVIS_DEBIAN_BUILD_DIR}"/ - \
-	| tar xf - -C "${TRAVIS_DEBIAN_TARGET_DIR}" --strip-components=1
-
 if [ "${TRAVIS_DEBIAN_AUTOPKGTEST}" = "true" ]
 then
 	Info "Running autopkgtests"
@@ -383,6 +378,11 @@ then
 fi
 EOF
 fi
+
+Info "Copying build artefacts to ${TRAVIS_DEBIAN_TARGET_DIR}"
+mkdir -p "${TRAVIS_DEBIAN_TARGET_DIR}"
+docker cp "$(cat "${CIDFILE}")":"${TRAVIS_DEBIAN_BUILD_DIR}"/ - \
+	| tar xf - -C "${TRAVIS_DEBIAN_TARGET_DIR}" --strip-components=1
 
 Info "Build successful"
 docker start "$(cat "${CIDFILE}")" >/dev/null
