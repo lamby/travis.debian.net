1e7660ef9663b0aea0e13f3ad45d6318bcd6a599
Chris Lamb <lamby@debian.org>
2018-04-05 11:11:01 +0100

    Blow up early if we don' thave TRAVIS_BUILD_NUMBER set.

--- script.sh
+++ script.sh
@@ -43,6 +43,12 @@ Indent () {
 	sed -e 's@^@  @g' "${@}"
 }
 
+if [ "${TRAVIS_BUILD_NUMBER:-}" = "" ]
+then
+	Error "TRAVIS_BUILD_NUMBER not set; are you running under TravisCI?"
+	exit 2
+fi
+
 ## Configuration ##############################################################
 
 if [ -f debian/changelog ]
